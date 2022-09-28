03e2cc8f2958eec93a90ce38b0b3649cdd585d82
iluckycat <andriei.voronin.2000@gmail.com>
2022-09-28 13:30:21 +0300

    First test update

--- test_smarthouse.py
+++ test_smarthouse.py
@@ -1,34 +1,18 @@
 import time
-
+from functions import get_names, is_light
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
-import webdriver_manager
 driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
-link="https://capsula.mail.ru/smart-home"
+link = "https://capsula.mail.ru/smart-home"
 obj = driver.get(link)
+def test_light_filter():
+    item = driver.find_element(By.XPATH, '//*[contains(@class, "sh-filter__group-item-text")]')
+    item.click()
+    time.sleep(5)
+    items = driver.find_element(By.XPATH, '//*[contains(@class,"sh-catalog__products")]')
+    items_child = items.find_elements(By.CSS_SELECTOR, '*')
+    names = get_names(items_child)
+    is_lght = is_light(names)
+    assert is_lght == True
 
-def get_attributes(driver, element) -> dict:
-    return driver.execute_script(
-        """
-        let attr = arguments[0].attributes;
-        let items = {}; 
-        for (let i = 0; i < attr.length; i++) {
-            items[attr[i].name] = attr[i].value;
-        }
-        return items;
-        """,
-        element
-    )
-
-
-item = driver.find_element(By.XPATH, '//*[contains(@class, "sh-filter__group-item-text")]')
-item.click()
-time.sleep(5)
-items = driver.find_element(By.XPATH, '//*[contains(@class,"sh-catalog__products")]')
-items_child = items.find_elements(By.CSS_SELECTOR, '*')
-for child in items_child:
-    name = child.text
-    if child.get_attribute('class') == 'sh-product__name' :
-        print(name)
-time.sleep(10)
